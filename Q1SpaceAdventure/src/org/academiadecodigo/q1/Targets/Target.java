0d3d97288b99e9db3845f48b697807489a136d5a
marsestarreja <marsestarreja@gmail.com>
2019-02-08 17:12:50 +0000

    added targets

---
+++ Q1SpaceAdventure/src/org/academiadecodigo/q1/Targets/Target.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.q1.Targets;
+
+import org.academiadecodigo.q1.Collidable;
+import org.academiadecodigo.q1.Destroyable;
+
+public class Target implements Collidable, Destroyable {
+
+    private TargetType targetType;
+    //private int hitPoints;
+
+
+    @Override
+    public void collide() {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ Q1SpaceAdventure/src/org/academiadecodigo/q1/Targets/TargetType.java
@@ -0,0 +1,6 @@
+package org.academiadecodigo.q1.Targets;
+
+public enum TargetType {
+
+    ASTEROID
+}
